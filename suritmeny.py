9ca6782f533c58b77057a0ca7ca772aeb1928a07
pulsztercsaba <pulsztercsaba7@gmail.com>
2020-11-25 20:23:21 +0000

    Initial commit

---
+++ suritmeny.py
@@ -0,0 +1,18 @@
+# 1. True or False
+
+szam = 2
+if szam % 2 == 0:
+    print("páros")
+else:
+    print("páratlan")
+
+# 2. def
+
+def triangle(a,b,c):
+    print("ker: ",a+b+c)
+a = input("a oldal: ")
+b = input("b oldal: ")
+c = input("c oldal: ")
+
+triangle(a,b,c)
+
